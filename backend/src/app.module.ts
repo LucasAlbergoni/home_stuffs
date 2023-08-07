81caf0ff24619962dbcd200d1259f8f42ac2a033
Lucas Albergoni <lucas.albergoni@gmail.com>
2023-08-06 23:48:03 -0300

    aula 2

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThingsModule } from './things/things.module';
+import { CategorysModule } from './categorys/categorys.module';
 
 @Module({
-  imports: [],
+  imports: [ThingsModule, CategorysModule],
   controllers: [AppController],
   providers: [AppService],
 })
